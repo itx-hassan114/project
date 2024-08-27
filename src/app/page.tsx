90373a2ee63b47c46e97293e03926e2306ecd1ff
Your Name <you@example.com>
2024-08-27 06:47:53 -0700

    updatess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react"
 
 const page = () => {
   return (
-    <div>Muhammad Hassan Saeed</div>
-  )
-}
+    <div>
+      <figure className="md:flex bg-yellow-100 rounded-x5 p-20 md:p-300 dark:bg-slate-1000">
+        <img className="w-80 h-60 md:w-60 md:rounded-none rounded-full mx-auto" src="/hassann.jpg" alt="" width="100" height="512" />
+        <div className="pt-6 md:p-40 text-center md:text-left space-y-2">
+          <blockquote>
+            <p className="text-lg font-large">
+              “ Aslam_ul_Alikum. I am Hassan.I am a student. I am learned AI course. And grow up in the country ,.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+               Muhammad Hassan Saeed
+            </div>
+            <div className="text-slate-1000 dark:text-slate-600">
+              Artificial Intelligence, (Student)
+            </div>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+  );
+};
+
 export default page
